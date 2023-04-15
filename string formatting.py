ccc638d6b88630d7d1704202f4bba6a68488f97b
MeeraNair07 <meera07nair@gmail.com>
2023-04-15 10:33:14 +0300

    string formatting

---
+++ string formatting.py
@@ -0,0 +1,15 @@
+# This prints out "Hello, John!"
+name = "John"
+print("Hello, %s!" % name)
+
+# %s - String (or any object with a string representation, like numbers)
+# %d - Integers
+# %f - Floating point numbers
+# %.<number of digits>f - Floating point numbers with a fixed amount of digits to the right of the dot.
+# %x/%X - Integers in hex representation (lowercase/uppercase)
+
+# You will need to write a format string which prints out the data using the following syntax: Hello John Doe. Your current balance is $53.44.
+data = ("John", "Doe", 53.44)
+format_string = "Hello %s %s. Your current balance is $%s."
+
+print(format_string % data)
